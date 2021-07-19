d264f0e6f86f42819e2570b130f506a0394b8348
script-money <hello@script.money>
2021-07-19 22:09:30 +0800

    🔧 add vite storybook tailwindcss ts react

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import { Button } from "./stories/Button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App text-center">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <Button
+            primary
+            label={`count is: ${count}`}
+            size="large"
+            onClick={() => setCount(count => count + 1)}
+          ></Button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a className="App-link text-main" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh({
+    // Exclude storybook stories and node_modules
+    exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+    // Only .tsx files
+    include: '**/*.tsx'
+  })]
+})
